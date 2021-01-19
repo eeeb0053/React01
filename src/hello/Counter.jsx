4cc5b93f476e398477757ce03e2e1c4acbc262f9
eeeb0053 <eeeb0053@gmail.com>
2021-01-19 15:56:16 +0900

    21/01/19

---
+++ src/hello/Counter.jsx
@@ -0,0 +1,12 @@
+import React, { useState } from 'react'
+
+const Counter = () => {
+    const [ number, serNumber ] = useState(0)
+    return <>
+    <h1>카운터</h1>
+    <h2>{number}</h2>
+    <button onClick = {() => setNumber(number+1)}> + </button>
+    <button onClick = {() => setNumber(number-1)}> - </button>
+    </>
+}
+export default Counter
